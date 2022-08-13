fa54be6ae6cb4bdd705ae125d81c856a62b7e685
Mikko <mkarvonen34@gmail.com>
2022-08-13 13:57:38 +0300

    Add semi colons to Prettier

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,17 @@
-import express, { Application, Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
 
-const app: Application = express()
+const app: Application = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World')
-})
+    res.send('Hello World');
+});
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
